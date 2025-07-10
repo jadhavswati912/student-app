a9b1666dc211c77f72ce0a2357292224dd80957d
Jadhav Swati <jadhavswati912@gmail.com>
2025-07-10 19:16:47 +0530

    update app page and bootstrap package

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { CommonModule, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule,CommonModule,NgIf],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'students-app';
+  title = 'student-app-1';
+  student = {
+    fullName: '',
+    email: ''
+  };
+  
+  onSubmit(form: any) {
+    if (form.valid) {
+      console.log('Form Data:', this.student);
+      alert('Registration Successful!');
+
+      // Reset the form after submission
+      form.reset();
+    }
+  }
 }
